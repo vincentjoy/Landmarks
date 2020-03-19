150eff516dfd610035a92f6e45d1d957edfab33e
Vincent Joy <joy.vincent@rehlat.com>
2020-03-19 13:52:37 +0530

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Vincent Joy on 19/03/20.
+//  Copyright © 2020 Vincent. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
