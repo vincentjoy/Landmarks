f7fe9e79e890e32931e33d71a0757af3a624dab1
Vincent Joy <vincent.joy@olaelectric.com>
2022-05-13 19:32:48 +0530

    Created models and added images for the second tutorial - Building lists and Navigation

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,31 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by AC02Z3225LVCG on 13/05/22.
+//  Copyright © 2022 Vincent. All rights reserved.
+//
+
+import Foundation
+
+func load<T: Decodable>(_ fileName: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: fileName, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(fileName) in main bundle")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(fileName) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(fileName) as \(T.self):\n\(error)")
+    }
+}
