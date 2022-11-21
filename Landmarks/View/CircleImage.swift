09069dd206ec5adcf7b2661f0a94318e18c5e2dc
Vincent Joy <vincent.joy@olaelectric.com>
2022-11-21 20:22:54 +0530

    Passing Data into Child Views

--- Landmarks/View/CircleImage.swift
+++ Landmarks/View/CircleImage.swift
@@ -9,8 +9,11 @@
 import SwiftUI
 
 struct CircleImage: View {
+    
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock")
+        image
             .clipShape(Circle())
             .overlay {
                 Circle().stroke(.white, lineWidth: 4)
@@ -21,6 +24,6 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
